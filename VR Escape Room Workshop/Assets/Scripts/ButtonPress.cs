c61f1f532271e38af07c6211a5ef6e270c48ebc8
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-12-08 02:41:23 -0700

    First Big

---
+++ VR Escape Room Workshop/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public Transform downTransform;
+    public Transform buttonMesh;
+
+    public GameObject lockPad;
+    public string key;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            lockPad.SendMessage("KeyEntry", key);
+            buttonMesh.position = downTransform.position;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            buttonMesh.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ VR Escape Room Workshop/Assets/Scripts/Lock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Lock : MonoBehaviour
+{
+    public UnityEvent unlock;
+    public string code = "ABD";
+    string currentCode;
+
+    public Text m_codeDisplay;
+    void KeyEntry(string key)
+    {
+        currentCode += key;
+        if(currentCode == code)
+        {
+            m_codeDisplay.text = "Code: " + currentCode;
+            unlock.Invoke();
+        }
+        else if(currentCode.Length == code.Length)
+        {
+            currentCode = "";
+            m_codeDisplay.text = "Code: " + currentCode;
+        }
+        else
+        {
+            m_codeDisplay.text = "Code: " + currentCode;
+        }
+    }
+}
